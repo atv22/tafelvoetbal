1585d86c71703f6102edab53d724e8f6624640a0
Johighnes <johannesrenders@yahoo.ca>
2025-11-26 19:52:49 +0100

    Verwijder import van niet-bestaande elo_calculation uit app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import time
 from datetime import date, datetime
 import firestore_service as db # Use Firestore
 from styles import setup_page
-from utils_new_elo import elo_calculation
 from utils import add_name, get_download_filename
 import plotly.express as px
 import plotly.graph_objects as go
